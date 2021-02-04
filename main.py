202109b9980ce175aab6f1730f9a4757802b7f32
kicysh <kicysh@protonmail.com>
2021-02-04 20:27:52 +0900

    add file

--- main.py
+++ main.py
@@ -1,9 +1,89 @@
 # 1
+def problems_1():
+    pass
+
 
 # 2
+def problems_2():
+    s = input()
+    ans = 0
+    while 'W' in s:
+        index = s.index('W')
+        ans += index
+        s = s[:index] + s[index+1:]
+    print(ans)
+
 
 # 3
+def problems_3():
+    pass
+
 
 # 4
+def problems_4():
+    s = input()
+    index = 0
+    if len(s)%2==0:
+        index = 2
+    else:
+        index = 1
+    ans = 0
+    for i in range(len(s)//2):
+        ii = (len(s)-index-i*2)//2
+        if s[:ii] == s[ii:ii*2]:
+            ans = len(s[:ii])*2
+            break
+    print(ans)
+
 
 # 5
+def problems_5():
+    n = int(input())
+    print(n*(n-1)//2)
+
+
+# 6
+def problems_6():
+    pass
+
+
+# 7
+def problems_7():
+    n = int(input())
+    s = input()
+    ans = 0
+    for i in range(1,n-1):
+        c = set(list(s[:i])) & set(list(s[i:]))
+        if ans < len(c):
+            ans = len(c)
+    print(ans)
+
+
+# 8
+def problems_8():
+    input()
+    a = [int(i)//400 for i in input().split()]
+    cnt,ans = 0,[]
+    for i in a:
+        if i > 7:
+            cnt += 1
+        else:
+            ans.append(i)
+    ans = len(set(ans))
+    print('{} {}'.format(max(1,ans),ans+cnt))
+
+# 9
+def problems_9():
+    a,b,c = map(int,input().split())
+    ab = a%b
+    ans = 'NO'
+    for i in range(b):
+        aa = ab*i%b
+        if aa==c:
+            ans = 'YES'
+            break
+    print(ans)
+
+# 10
+def problems_10():
+    pass
