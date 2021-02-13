d3c4af0b2436ecd9e6d864ef74c93c09e119155e
kicysh <kicysh@protonmail.com>
2021-02-13 17:36:33 +0900

    add: 46

--- main.py
+++ main.py
@@ -538,3 +538,16 @@ def problem_45():
     ans = min(n,m//2)
     ans +=(m-ans*2)//4
     print(ans)
+
+
+# 46
+def problem_46():
+    from collections import Counter
+    s = input()
+    c = Counter(s).values()
+    if (len(c)<3) & (max(c)>1):
+        print('NO')
+    elif (max(c)-min(c)>1):
+        print('NO')
+    else:
+        print('YES')
