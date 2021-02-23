a5f3efe32d04bba8643d9d036924824d76563eb5
kicysh <kicysh@protonmail.com>
2021-02-24 02:17:32 +0900

    add: 2

--- hard.py
+++ hard.py
@@ -19,3 +19,21 @@ def problem_1():
             ans[p+1]+=(l+2)//2
             r,l = 0,0 
     print(('{} '*len(ans[:-1])).format(*ans[:-1])+str(ans[-1]))
+
+
+# 2: jsc2019_qual_b
+def problem_2():
+    n,k = map(int,input().split())
+    a = list(map(int,input().split()))
+    INF = 10**9+7
+    ans = 0
+    for ai in range(n-1,-1,-1):
+        cnt = 0
+        for aj in range(n-1,-1,-1):
+            if ai==aj:
+                ans = (ans+cnt*k)%INF
+            elif a[ai]>a[aj]:
+                cnt +=1
+        ans += cnt*k*(k-1)//2
+        ans %=INF
+    print(ans)
